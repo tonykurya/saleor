d0ce114bf8165274b8e2917a1097c68d26df1d28
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-10-17 23:35:35 +0200

    Drop django payments

--- saleor/dashboard/views.py
+++ saleor/dashboard/views.py
@@ -35,7 +35,7 @@ def superuser_required(
 def index(request):
     paginate_by = 10
     orders_to_ship = Order.objects.to_ship().select_related(
-        'user').prefetch_related('lines', 'payments')
+        'user').prefetch_related('lines', 'payment_methods')
     payments = PaymentMethod.objects.filter(
         is_active=True, charge_status=ChargeStatus.NOT_CHARGED
     ).order_by('-created')
